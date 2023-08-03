dffc6e7a5fa0f5dc729a601869b3ad3e0500c45a
Bekiboo <julien.connault@gmail.com>
2023-08-03 13:00:01 +0300

    Authentication

--- src/lib/components/AnimatedRoutes.svelte
+++ src/lib/components/AnimatedRoutes.svelte
@@ -1,10 +1,11 @@
 <script>
 	import { fly } from 'svelte/transition';
 	import { page } from '$app/stores';
+	export let direction = '-100%';
 </script>
 
 {#key $page.url}
-	<div in:fly={{ x: '-100%', duration: 500 }}>
+	<div in:fly={{ x: 100, duration: 100 }}>
 		<slot />
 	</div>
 {/key}
