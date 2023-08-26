f8600e2d440ca4a9797ad14564bff4c3a09e0728
Bekiboo <julien.connault@gmail.com>
2023-08-26 17:34:00 +0300

    cleanup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,9 @@
 <script lang="ts">
-	import ThemeSwitch from './ThemeSwitch.svelte';
 </script>
 
 <header>
-	<!-- dark/light theme switch -->
-	<div class="flex justify-end">
-		<ThemeSwitch />
-	</div>
+	<div class="flex justify-end" />
 </header>
 
-<main>
-	<button class="mt-10 ml-20">Click Me</button>
-</main>
-
 <style>
-	main {
-		background-color: var(--primary);
-	}
 </style>
