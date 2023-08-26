dec564d28397a8a163b6f56807402584c6ac2f59
Bekiboo <julien.connault@gmail.com>
2023-08-26 08:44:37 +0300

    tiny correction

--- src/routes/projects/fkit/[username]/+page.ts
+++ src/routes/projects/fkit/[username]/+page.ts
@@ -1,21 +1,21 @@
-import { collection, getDocs, limit, query, where } from 'firebase/firestore';
-import { db } from '$lib/firebase';
-import { error } from '@sveltejs/kit';
+import { collection, getDocs, limit, query, where } from 'firebase/firestore'
+import { db } from '$lib/firebase'
+import { error } from '@sveltejs/kit'
 
-export const load = async ({ params }: any) => {
-	const collectionRef = collection(db, 'users');
+export const load = async ({ params }) => {
+	const collectionRef = collection(db, 'users')
 
-	const q = query(collectionRef, where('username', '==', params.username), limit(1));
-	const snapshot = await getDocs(q);
-	const exists = snapshot.docs[0]?.exists();
-	const data = snapshot.docs[0]?.data();
+	const q = query(collectionRef, where('username', '==', params.username), limit(1))
+	const snapshot = await getDocs(q)
+	const exists = snapshot.docs[0]?.exists()
+	const data = snapshot.docs[0]?.data()
 
 	if (!exists) {
-		throw error(404, 'that user does not exist!');
+		throw error(404, 'that user does not exist!')
 	}
 
 	if (!data.published) {
-		throw error(403, `The profile of @${data.username} is not public!`);
+		throw error(403, `The profile of @${data.username} is not public!`)
 	}
 
 	return {
@@ -23,5 +23,5 @@ export const load = async ({ params }: any) => {
 		photoURL: data.photoURL,
 		bio: data.bio,
 		links: data.links ?? []
-	};
-};
+	}
+}
