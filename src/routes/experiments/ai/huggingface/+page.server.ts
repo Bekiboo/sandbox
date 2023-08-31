2ce5ad1b35f705f382363638de7827e7ab4447c1
Bekiboo <julien.connault@gmail.com>
2023-08-31 09:37:38 +0300

    add loading state

--- src/routes/experiments/ai/huggingface/+page.server.ts
+++ src/routes/experiments/ai/huggingface/+page.server.ts
@@ -3,6 +3,12 @@ import { HUGGINGFACE_KEY } from '$env/static/private'
 import type { Actions } from './$types'
 import { fail } from '@sveltejs/kit'
 
+const models = [
+	'Salesforce/blip-image-captioning-large',
+	'nlpconnect/vit-gpt2-image-captioning',
+	'noamrot/FuseCap_Image_Captioning'
+]
+
 export const actions = {
 	default: async ({ request }) => {
 		const data = await request.formData()
@@ -14,35 +20,20 @@ export const actions = {
 
 		const inference = new HfInference(HUGGINGFACE_KEY)
 
-		const models = [
-			{
-				name: 'Salesforce/blip-image-captioning-large',
-				url: 'https://huggingface.co/Salesforce/blip-image-captioning-large'
-			},
-			{
-				name: 'nlpconnect/vit-gpt2-image-captioning',
-				url: 'https://huggingface.co/nlpconnect/vit-gpt2-image-captioning'
-			},
-			{
-				name: 'noamrot/FuseCap_Image_Captioning',
-				url: 'https://huggingface.co/noamrot/FuseCap_Image_Captioning'
-			}
-		]
-
 		const response = await fetch(imgUrl as string)
 		const imageBlob = await response.blob()
 
 		const inferencePromises = models.map(async (model) => {
-			const result = inference.imageToText({
+			const result = await inference.imageToText({
 				data: imageBlob,
-				model: model.name
+				model: model // Use the current model's name
 			})
-			return result.then((caption) => ({ modelName: model.name, caption }))
+			return { model, caption: result }
 		})
 
 		// Wait for all inference promises to complete
 		const results = await Promise.all(inferencePromises)
 
-		return { captions: results }
+		return { results }
 	}
 } satisfies Actions
--- src/routes/experiments/ai/huggingface/+page.svelte
+++ src/routes/experiments/ai/huggingface/+page.svelte
@@ -4,31 +4,33 @@
 
 	export let form: ActionData
 
-	let caption = ''
-
-	let loading = false
-
-	$: console.log(form?.captions)
+	let isLoading = false
 </script>
 
-<div class="flex flex-col items-center justify-center h-screen px-16">
-	<h1 class="text-2xl font-bold">Image to Caption</h1>
-	<h2>A HuggingFace model comparator</h2>
-	<form method="POST" use:enhance class="flex flex-col gap-4 mt-12">
+<div class="flex flex-col items-center h-screen max-w-xs py-12 mx-auto md:max-w-lg">
+	<h1 class="text-2xl font-bold md:text-4xl">Image to Caption</h1>
+	<h2 class="mt-1 md:text-xl text-secondary">A HuggingFace model comparator</h2>
+	<form
+		method="POST"
+		use:enhance={() => {
+			isLoading = true
+			return async ({ result, update }) => {
+				update()
+				isLoading = false
+			}
+		}}
+		class="flex flex-col w-full gap-4 mt-12"
+		class:isLoading
+	>
 		{#if form?.missing}
-			<p class="text-red-500">No URL provided</p>
+			<p class="text-error">No URL provided</p>
 		{/if}
-		<input
-			type="text"
-			name="imgUrl"
-			placeholder="Type here"
-			class="max-w-6xl w-96 input input-bordered"
-		/>
+		<input type="text" name="imgUrl" placeholder="Type here" class="w-full input input-bordered" />
 		<p>
 			{form?.imgUrl ?? ''}
 		</p>
-		<button type="submit" class="btn btn-primary">
-			{#if !loading}
+		<button type="submit" class="w-full btn btn-primary">
+			{#if !isLoading}
 				Get Caption
 			{:else}
 				<span class="loading loading-spinner loading-lg" />
@@ -37,12 +39,23 @@
 	</form>
 
 	<p>
-		{#if form?.captions}
-			{#each form?.captions as caption}<div class="mt-4">
-					<h3 class="text-xl">{caption?.modelName}</h3>
-					<p class="text-white">{caption?.caption.generated_text}</p>
+		{#if form?.results && !isLoading}
+			{#each form?.results as result}<div class="mt-4">
+					<a
+						target="_blank"
+						href="https://huggingface.co/{result?.model}"
+						class="text-xl link link-secondary">{result?.model}</a
+					>
+					<p>{result?.caption.generated_text}</p>
 				</div>
 			{/each}
 		{/if}
 	</p>
 </div>
+
+<style>
+	.isLoading {
+		pointer-events: none;
+		opacity: 0.5;
+	}
+</style>
