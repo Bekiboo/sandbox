ffdf6af025d50bc59d984c14827de5d50a1e29ac
Bekiboo <julien.connault@gmail.com>
2023-10-29 22:48:16 +0300

    Keyboard Handler

---
+++ src/routes/experiments/three-js/moving-cube/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { Canvas } from '@threlte/core'
+	import Scene from './Scene.svelte'
+</script>
+
+<div class="relative">
+	<div class="absolute left-2 top-2">
+		<div class="flex justify-center w-full">
+			<kbd class="kbd">W</kbd>
+		</div>
+		<div class="flex justify-center w-full">
+			<kbd class="kbd">A</kbd>
+			<kbd class="kbd">S</kbd>
+			<kbd class="kbd">D</kbd>
+		</div>
+		<div class="flex justify-center w-full mt-2">
+			<kbd class="kbd">Space</kbd>
+		</div>
+	</div>
+
+	<div class="h-screen w-sceen">
+		<Canvas>
+			<Scene />
+		</Canvas>
+	</div>
+</div>
--- src/routes/experiments/three-js/moving-cube/Scene.svelte
+++ src/routes/experiments/three-js/moving-cube/Scene.svelte
@@ -3,16 +3,17 @@
 	import { Grid, interactivity, OrbitControls } from '@threlte/extras'
 	import { BufferGeometry, Float32BufferAttribute, MathUtils } from 'three'
 	import { DEG2RAD } from 'three/src/math/MathUtils'
+	import { handleKeysDown, handleKeysUp } from './keyboardInputHandler'
 
 	interactivity()
 
-	let cameraPos: [x: number, y: number, z: number] = [2, 2, 5]
+	let cameraPos: [x: number, y: number, z: number] = [10, 20, 40]
 	let lightPos: [x: number, y: number, z: number] = [5, 10, 5]
 
 	const count = 100
 	const distance = 2
 
-	const GRAVITY = 0.01
+	const GRAVITY = 0.03
 
 	const points = new Float32Array(count * 3)
 	for (let i = 0; i < points.length; i++) {
@@ -22,14 +23,10 @@
 	const geometry = new BufferGeometry()
 	geometry.setAttribute('position', new Float32BufferAttribute(points, 3))
 
-	function isGrounded() {
-		return player.pos.y === -0.25
-	}
-
 	let player = {
 		pos: {
 			x: 0,
-			y: -0.25,
+			y: 0,
 			z: 0
 		},
 		velocity: {
@@ -39,78 +36,46 @@
 		}
 	}
 
-	let keyPressed: string[] = []
+	let keysPressed: string[] = []
 
 	function handleKeyDown(e: KeyboardEvent) {
-		if (e.key === 'd' || e.key === 'D' || e.key === 'ArrowRight') {
-			keyPressed.push('right')
-		}
-		if (e.key === 'a' || e.key === 'A' || e.key === 'ArrowLeft') {
-			keyPressed.push('left')
-		}
-		if (e.key === 'w' || e.key === 'W' || e.key === 'ArrowUp') {
-			keyPressed.push('forward')
-		}
-		if (e.key === 's' || e.key === 'S' || e.key === 'ArrowDown') {
-			keyPressed.push('backward')
-		}
-		if (e.key === ' ') {
-			keyPressed.push('jump')
-		}
+		keysPressed = handleKeysDown(e, keysPressed)
 	}
 
 	function handleKeyUp(e: KeyboardEvent) {
-		if (e.key === 'd' || e.key === 'D' || e.key === 'ArrowRight') {
-			keyPressed = keyPressed.filter((key) => key !== 'right')
-		}
-		if (e.key === 'a' || e.key === 'A' || e.key === 'ArrowLeft') {
-			keyPressed = keyPressed.filter((key) => key !== 'left')
-		}
-		if (e.key === 'w' || e.key === 'W' || e.key === 'ArrowUp') {
-			keyPressed = keyPressed.filter((key) => key !== 'forward')
-		}
-		if (e.key === 's' || e.key === 'S' || e.key === 'ArrowDown') {
-			keyPressed = keyPressed.filter((key) => key !== 'backward')
-		}
-		if (e.key === ' ') {
-			keyPressed = keyPressed.filter((key) => key !== 'jump')
-		}
+		keysPressed = handleKeysUp(e, keysPressed)
 	}
 
 	useFrame((state, delta) => {
-		if (keyPressed.includes('right')) {
+		if (keysPressed.includes('right')) {
 			player.pos.x += 0.1
 		}
-		if (keyPressed.includes('left')) {
+		if (keysPressed.includes('left')) {
 			player.pos.x -= 0.1
 		}
-		if (keyPressed.includes('forward')) {
+		if (keysPressed.includes('forward')) {
 			player.pos.z -= 0.1
 		}
-		if (keyPressed.includes('backward')) {
+		if (keysPressed.includes('backward')) {
 			player.pos.z += 0.1
 		}
-		if (keyPressed.includes('jump')) {
-			// if (isGrounded()) {
-			player.velocity.y = -0.2
-			// }
+		if (keysPressed.includes('jump')) {
+			player.velocity.y = -0.3
 		}
 		player.pos.y -= player.velocity.y
 		player.velocity.y += GRAVITY
 
-		if (player.pos.y < -0.25) {
-			player.pos.y = -0.25
+		if (player.pos.y < 0) {
+			player.pos.y = 0
 		}
 
 		// cameraPos[0] = player.pos.x + 2
 		// cameraPos[1] = player.pos.y + 2
 		// cameraPos[2] = player.pos.z + 5
 	})
-
-	// $: console.log(player.velocity.y)
 </script>
 
-<T.PerspectiveCamera position={cameraPos} makeDefault fov={75} near={0.1} far={1000000}>
+<T.PerspectiveCamera position={cameraPos} makeDefault fov={15} near={0.1} far={1000000}>
 	<OrbitControls maxPolarAngle={85 * DEG2RAD} minPolarAngle={20 * DEG2RAD} />
 </T.PerspectiveCamera>
 
@@ -134,8 +99,8 @@
 </T.Mesh>
 
 <!-- GROUND -->
-<T.Mesh position={[0, -1, 0]} receiveShadow>
-	<T.BoxGeometry args={[30, 1, 30]} />
+<T.Mesh position={[0, -1.25, 0]} receiveShadow>
+	<T.BoxGeometry args={[30, 2, 30]} />
 	<T.MeshStandardMaterial color={'#fff'} />
 </T.Mesh>
 
