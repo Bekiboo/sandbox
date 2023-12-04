6a712e150d21853d7e5bd85178ab399444cc0327
Bekiboo <julien.connault@gmail.com>
2023-12-04 23:32:33 +0300

    Advent of Svelte - Day 1

---
+++ src/routes/projects/advent-of-svelte/+layout.svelte
@@ -0,0 +1,32 @@
+<div class="w-screen h-full min-h-screen bg-[#0D121D]">
+	<nav class="h-8 bg-[#FF3E00]">
+		<ul class="flex">
+			<span class="font-bold text-lg text-white">Days</span>
+			<li>
+				<a
+					class="font-bold text-lg text-white px-2 hover:text-black"
+					href="/projects/advent-of-svelte/day1">1</a
+				>
+			</li>
+			<li>
+				<a
+					class="font-bold text-lg text-white px-2 hover:text-black"
+					href="/projects/advent-of-svelte/day2">2</a
+				>
+			</li>
+			<li>
+				<a
+					class="font-bold text-lg text-white px-2 hover:text-black"
+					href="/projects/advent-of-svelte/day3">3</a
+				>
+			</li>
+			<li>
+				<a
+					class="font-bold text-lg text-white px-2 hover:text-black"
+					href="/projects/advent-of-svelte/day4">4</a
+				>
+			</li>
+		</ul>
+	</nav>
+	<slot />
+</div>
