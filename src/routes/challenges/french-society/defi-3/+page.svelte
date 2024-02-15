bb5a53c7303b4dddbcbac610118dad44b6192737
Bekiboo <julien.connault@gmail.com>
2024-02-15 22:25:29 +0300

    improve styling

--- src/routes/challenges/french-society/defi-3/+page.svelte
+++ src/routes/challenges/french-society/defi-3/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import { goto } from '$app/navigation'
 	import { page } from '$app/stores'
+	import { quintInOut } from 'svelte/easing'
+	import { draw } from 'svelte/transition'
 
-	// tic tac toe
 	let board = Array(9).fill(null)
 	let xIsNext = true
 	let winner = null
@@ -52,9 +53,9 @@
 
 	$: status = winner ? `Winner: ${winner}` : `Next player: ${xIsNext ? 'X' : 'O'}`
 
-	$: draw = board.every((square) => square !== null)
+	$: drawBoard = board.every((square) => square !== null)
 
-	$: gameOver = winner || draw
+	$: gameOver = winner || drawBoard
 
 	$: gameOver && console.log('Game Over')
 
@@ -68,7 +69,38 @@
 	<div class="status">{status}</div>
 	<div class="board">
 		{#each board as square, i}
-			<div class="square" on:click={() => handleClick(i)}>{square}</div>
+			<button class="square" on:click={() => handleClick(i)}>
+				{#if square == 'X'}
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						class="rotate-45"
+						><path transition:draw={{ duration: 150, easing: quintInOut }} d="M5 12h14" /><path
+							transition:draw={{ duration: 150, delay: 150, easing: quintInOut }}
+							d="M12 5v14"
+						/></svg
+					>
+				{:else if square == 'O'}
+					<!-- SVG for a simple circle -->
+					<svg
+						data-slot="icon"
+						fill="none"
+						stroke-width="1.5"
+						stroke="currentColor"
+						viewBox="0 0 24 24"
+						xmlns="http://www.w3.org/2000/svg"
+						aria-hidden="true"
+					>
+						<circle transition:draw={{ duration: 300, easing: quintInOut }} cx="12" cy="12" r="9"
+						></circle>
+					</svg>
+				{/if}
+			</button>
 		{/each}
 	</div>
 </main>
@@ -78,6 +110,9 @@
 		display: grid;
 		grid-template-columns: repeat(3, 100px);
 		grid-template-rows: repeat(3, 100px);
+		outline: 5px solid #ccc;
+		width: min-content;
+		margin: auto;
 	}
 
 	.square {
@@ -85,7 +120,7 @@
 		justify-content: center;
 		align-items: center;
 		font-size: 3rem;
-		border: 1px solid #999;
+		border: 5px solid #ccc;
 	}
 
 	.status {
