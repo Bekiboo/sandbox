c7f11b43a1289c6adddb2a3193d768c3364d6944
Bekiboo <julien.connault@gmail.com>
2024-06-11 10:46:50 +0300

    chore: Add D20 and skill icons to DC20 character sheet

--- src/routes/projects/DC20-character-sheet/+page.svelte
+++ src/routes/projects/DC20-character-sheet/+page.svelte
@@ -2,6 +2,9 @@
 	import { writable } from 'svelte/store'
 	import { browser } from '$app/environment'
 	import Skill from './skill.svelte'
+	import D20 from './Icon-D20.svelte'
+	import SkillIcon from './Icon-Skill.svelte'
+	import IconDc20 from './Icon-DC20.svelte'
 
 	// Define the character sheet store
 	export const blankCS = {
@@ -11,16 +14,19 @@
 		ancestry: '',
 		level: 1,
 		combatMastery: '',
-		strength: 0,
+		might: 0,
 		agility: 0,
 		charisma: 0,
 		intelligence: 0,
-		prime: 0
+		prime: 0,
+		awareness: 0,
+		athletics: 0,
+		intimidation: 0,
+		investigation: 0
 	}
 
 	// Ensure the initial value is either an object or the default test object
-	const initialCS =
-		(browser && JSON.parse(localStorage.getItem('CharacterSheet') || blankCS.toString())) || blankCS
+	const initialCS = (browser && JSON.parse(localStorage.getItem('CharacterSheet'))) || blankCS
 
 	export const CS = writable(initialCS)
 
@@ -28,16 +34,18 @@
 		if (browser) localStorage.setItem('CharacterSheet', JSON.stringify(val))
 	})
 
-	$: $CS.prime = Math.max($CS.strength, $CS.agility, $CS.charisma, $CS.intelligence)
+	$: $CS.prime = Math.max($CS.might, $CS.agility, $CS.charisma, $CS.intelligence)
 	$: $CS.combatMastery = Math.round($CS.level / 2)
 </script>
+
+<IconDc20 size="12rem" />
 
 <div>Player name: <input type="text" bind:value={$CS.name} /></div>
 <div>Character name: <input type="text" bind:value={$CS.characterName} /></div>
 <div>Class & Subclass: <input type="text" bind:value={$CS.class} /></div>
 <div>Ancestry & Background: <input type="text" bind:value={$CS.ancestry} /></div>
 <div>Level: <input type="number" bind:value={$CS.level} /></div>
-<div>Combat Mastery: <input type="text" bind:value={$CS.combatMastery} /></div>
+<div>Combat Mastery: {$CS.combatMastery}</div>
 -----------------------
 <section>
 	<h2>Attributes & Skills</h2>
--- src/routes/projects/DC20-character-sheet/skill.svelte
+++ src/routes/projects/DC20-character-sheet/skill.svelte
@@ -1,41 +1,28 @@
 <script lang="ts">
+	import IconSkill from './Icon-Skill.svelte'
+
 	export let value = 0
 	export let skill = '__________'
 	export let total
 </script>
 
 <div class="flex items-center gap-4">
-	<div>
-		<button
-			class="w-4 h-4 border rounded-l-full rounded-b-full rotate-45 duration-100 {value >= 2
-				? 'bg-white'
-				: ''}"
-			on:click={() => (value != 2 ? (value = 2) : (value = 0))}
-		></button>
-		<button
-			class="w-4 h-4 border rounded-l-full rounded-b-full rotate-45 duration-100 {value >= 4
-				? 'bg-white'
-				: ''}"
-			on:click={() => (value != 4 ? (value = 4) : (value = 2))}
-		></button>
-		<button
-			class="w-4 h-4 border rounded-l-full rounded-b-full rotate-45 duration-100 {value >= 6
-				? 'bg-white'
-				: ''}"
-			on:click={() => (value != 6 ? (value = 6) : (value = 4))}
-		></button>
-		<button
-			class="w-4 h-4 border rounded-l-full rounded-b-full rotate-45 duration-100 {value >= 8
-				? 'bg-white'
-				: ''}"
-			on:click={() => (value != 8 ? (value = 8) : (value = 6))}
-		></button>
-		<button
-			class="w-4 h-4 border rounded-l-full rounded-b-full rotate-45 duration-100 {value >= 10
-				? 'bg-white'
-				: ''}"
-			on:click={() => (value != 10 ? (value = 10) : (value = 8))}
-		></button>
+	<div class="flex">
+		<button on:click={() => (value != 2 ? (value = 2) : (value = 0))}>
+			<IconSkill stroke="white" size="2rem" fill={value >= 2 ? '#ccc' : 'transparent'}></IconSkill>
+		</button>
+		<button on:click={() => (value != 4 ? (value = 4) : (value = 2))}>
+			<IconSkill stroke="white" size="2rem" fill={value >= 4 ? '#ccc' : 'transparent'}></IconSkill>
+		</button>
+		<button on:click={() => (value != 6 ? (value = 6) : (value = 4))}>
+			<IconSkill stroke="white" size="2rem" fill={value >= 6 ? '#ccc' : 'transparent'}></IconSkill>
+		</button>
+		<button on:click={() => (value != 8 ? (value = 8) : (value = 6))}>
+			<IconSkill stroke="white" size="2rem" fill={value >= 8 ? '#ccc' : 'transparent'}></IconSkill>
+		</button>
+		<button on:click={() => (value != 10 ? (value = 10) : (value = 8))}>
+			<IconSkill stroke="white" size="2rem" fill={value >= 10 ? '#ccc' : 'transparent'}></IconSkill>
+		</button>
 	</div>
 
 	<div class="w-8 text-center border">
