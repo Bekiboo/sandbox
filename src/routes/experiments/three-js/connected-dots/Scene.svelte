eb664eacc23c2326fef07d5ed434e511e108bba9
Bekiboo <julien.connault@gmail.com>
2024-09-21 09:56:04 +0300

    Fix connected-dots

--- src/routes/experiments/three-js/connected-dots/Scene.svelte
+++ src/routes/experiments/three-js/connected-dots/Scene.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import { T, useFrame } from '@threlte/core'
-	import { interactivity, OrbitControls } from '@threlte/extras'
+	import { T, useTask, useThrelte } from '@threlte/core';
+	import { interactivity, OrbitControls } from '@threlte/extras';
 	import {
 		BufferGeometry,
 		Float32BufferAttribute,
@@ -8,53 +8,52 @@
 		Mesh,
 		Points,
 		PointsMaterial
-	} from 'three'
-	import { DEG2RAD } from 'three/src/math/MathUtils'
+	} from 'three';
+	import { DEG2RAD } from 'three/src/math/MathUtils.js';
 
-	interactivity()
+	interactivity();
 
-	let cameraPosition: [x: number, y: number, z: number] = [2, 3, 5]
-	let lightPosition: [x: number, y: number, z: number] = [5, 10, 5]
+	let cameraPosition: [x: number, y: number, z: number] = [2, 3, 5];
+	let lightPosition: [x: number, y: number, z: number] = [5, 10, 5];
 
-	const count = 1000
-	const distance = 2
+	const count = 1000;
+	const distance = 2;
 
-	const points = new Float32Array(count * 3)
+	const points = new Float32Array(count * 3);
 	for (let i = 0; i < points.length; i++) {
-		points[i] = MathUtils.randFloatSpread(distance * 2)
+		points[i] = MathUtils.randFloatSpread(distance * 2);
 	}
 
-	const geometry = new BufferGeometry()
-	geometry.setAttribute('position', new Float32BufferAttribute(points, 3))
+	const geometry = new BufferGeometry();
+	geometry.setAttribute('position', new Float32BufferAttribute(points, 3));
 
 	// const pointMaterial = new PointsMaterial({
 	// 	color: 0xff0000,
 	// 	size: 0.1
 	// })
 
-	let direction = 1
+	let direction = 1;
 
-	useFrame((state, delta) => {
-		const time = state.clock.getElapsedTime()
+	const { camera, scene } = useThrelte();
 
-		if (time > 2) {
-			direction *= -1
-			state.clock.elapsedTime = 0
+	useTask((delta) => {
+		if (Math.random() < 0.01) {
+			direction *= -1;
 		}
 
-		const positions = geometry.attributes.position.array as Float32Array
+		const positions = geometry.attributes.position.array as Float32Array;
 		for (let i = 0; i < positions.length; i += 3) {
-			const x = positions[i]
-			const y = positions[i + 1]
-			const z = positions[i + 2]
+			const x = positions[i];
+			const y = positions[i + 1];
+			const z = positions[i + 2];
 
-			positions[i] = x + Math.sin(x) * 0.05 * direction
-			positions[i + 1] = y + Math.sin(y) * 0.01 * direction
-			positions[i + 2] = z + Math.sin(z) * 0.01 * direction
+			positions[i] = x + Math.sin(x) * 0.05 * direction;
+			positions[i + 1] = y + Math.sin(y) * 0.01 * direction;
+			positions[i + 2] = z + Math.sin(z) * 0.01 * direction;
 		}
 
-		geometry.attributes.position.needsUpdate = true
-	})
+		geometry.attributes.position.needsUpdate = true;
+	});
 </script>
 
 <T.PerspectiveCamera position={cameraPosition} makeDefault fov={75} near={0.1} far={1000000}>
